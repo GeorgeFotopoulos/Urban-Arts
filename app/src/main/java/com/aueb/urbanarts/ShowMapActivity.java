ae35d37b1bed4d24d3f875ee35975df76ecc977a
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-05-09 16:45:24 +0300

    Map Made!

--- app/src/main/java/com/aueb/urbanarts/ShowMapActivity.java
+++ app/src/main/java/com/aueb/urbanarts/ShowMapActivity.java
@@ -4,15 +4,39 @@ package com.aueb.urbanarts;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.google.android.gms.maps.CameraUpdateFactory.newLatLng;
 
 
-public class ShowMapActivity extends AppCompatActivity {
+public class ShowMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_map);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng position = new LatLng(37.984513, 23.727946);
+        map.addMarker(new MarkerOptions().position(position).title("Marker"));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 15.0f));
     }
 }
