d6c966f239032a06168fe21e956689699bcde190
GeorgeFotopoulos <giorgos.fotopoulos7@gmail.com>
2020-05-10 16:44:19 +0300

    Menu Fixed

--- app/src/main/java/com/aueb/urbanarts/HomePage.java
+++ app/src/main/java/com/aueb/urbanarts/HomePage.java
@@ -14,23 +14,23 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        ImageButton filter = (ImageButton) findViewById(R.id.filters);
-        filter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(HomePage.this, ReportUser.class);
-                HomePage.this.startActivity(myIntent);
-            }
-        });
-
-        ImageButton live = (ImageButton) findViewById(R.id.livefeed);
-        live.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(HomePage.this, ShowMapActivity.class);
-                HomePage.this.startActivity(myIntent);
-            }
-        });
+        //ImageButton filter = (ImageButton) findViewById(R.id.filters);
+        //filter.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent myIntent = new Intent(HomePage.this, ReportUser.class);
+        //        HomePage.this.startActivity(myIntent);
+        //    }
+        //});
+//
+        //ImageButton live = (ImageButton) findViewById(R.id.liveFeed);
+        //live.setOnClickListener(new View.OnClickListener(){
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent myIntent = new Intent(HomePage.this, ShowMapActivity.class);
+        //        HomePage.this.startActivity(myIntent);
+        //    }
+        //});
     }
 
 
