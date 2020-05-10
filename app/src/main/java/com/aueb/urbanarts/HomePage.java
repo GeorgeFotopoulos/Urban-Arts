01c2c65dcda7071f15bb37179716de2b3fab8f5f
GeorgeFotopoulos <giorgos.fotopoulos7@gmail.com>
2020-05-10 18:30:11 +0300

    Sign Up added

--- app/src/main/java/com/aueb/urbanarts/HomePage.java
+++ app/src/main/java/com/aueb/urbanarts/HomePage.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class HomePage extends AppCompatActivity {
@@ -15,23 +14,23 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-//        ImageView filter = (ImageView) findViewById(R.id.filters);
-//        filter.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent myIntent = new Intent(HomePage.this, ReportUser.class);
-//                HomePage.this.startActivity(myIntent);
-//            }
-//        });
-
-        ImageView live = (ImageView) findViewById(R.id.liveFeed);
-        live.setOnClickListener(new View.OnClickListener(){
+        ImageView register = (ImageView) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(HomePage.this, ShowMapActivity.class);
+                Intent myIntent = new Intent(HomePage.this, SignUp.class);
                 HomePage.this.startActivity(myIntent);
             }
         });
+
+        //ImageButton live = (ImageButton) findViewById(R.id.liveFeed);
+        //live.setOnClickListener(new View.OnClickListener(){
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent myIntent = new Intent(HomePage.this, ShowMapActivity.class);
+        //        HomePage.this.startActivity(myIntent);
+        //    }
+        //});
     }
 
 
