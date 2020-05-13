067777431bed5238181e9500bfd484ef60f71dbf
GeorgeFotopoulos <giorgos.fotopoulos7@gmail.com>
2020-05-13 19:51:16 +0300

    Register Authentication added

--- app/src/main/java/com/aueb/urbanarts/HomePage.java
+++ app/src/main/java/com/aueb/urbanarts/HomePage.java
@@ -1,39 +1,78 @@
 package com.aueb.urbanarts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    String tempStr = "";
+    TextView temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        ImageView register = (ImageView) findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(HomePage.this, SignUp.class);
-                HomePage.this.startActivity(myIntent);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
 
-         ImageView signIn = (ImageView) findViewById(R.id.logIn);
-         signIn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent myIntent = new Intent(HomePage.this, LogIn.class);
-                 HomePage.this.startActivity(myIntent);
-             }
-         });
+        ImageView signIn = findViewById(R.id.logIn);
+        if (mAuth.getCurrentUser() != null) {
+            signIn.setImageResource(R.drawable.log_out);
+            tempStr = "Log Out";
+            temp = findViewById(R.id.tv_logIn);
+            temp.setText(tempStr);
+            signIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mAuth.signOut();
+                    // kane log out
+                    Intent myIntent = new Intent(HomePage.this, HomePage.class);
+                    HomePage.this.startActivity(myIntent);
+                }
+            });
+        } else {
+            signIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent myIntent = new Intent(HomePage.this, LogIn.class);
+                    HomePage.this.startActivity(myIntent);
+                }
+            });
+        }
 
-        ImageView filters = (ImageView) findViewById(R.id.filters);
-        filters.setOnClickListener(new View.OnClickListener(){
+        ImageView register = findViewById(R.id.register);
+        if (mAuth.getCurrentUser() != null) {
+            register.setImageResource(R.drawable.edit_account);
+            tempStr = "Edit Account";
+            temp = findViewById(R.id.tv_register);
+            temp.setText(tempStr);
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Stelne me sto EditAccountActivity
+                    // Intent myIntent = new Intent(HomePage.this, EditAccountActivity.class);
+                    // HomePage.this.startActivity(myIntent);
+                }
+            });
+        } else {
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent myIntent = new Intent(HomePage.this, SignUp.class);
+                    HomePage.this.startActivity(myIntent);
+                }
+            });
+        }
+
+        ImageView filters = findViewById(R.id.filters);
+        filters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(HomePage.this, ArtistProfileActivity.class);
@@ -41,8 +80,8 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
-        ImageView live = (ImageView) findViewById(R.id.liveFeed);
-        live.setOnClickListener(new View.OnClickListener(){
+        ImageView live = findViewById(R.id.liveFeed);
+        live.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(HomePage.this, Feed.class);
@@ -50,8 +89,8 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
-        ImageView createPost = (ImageView) findViewById(R.id.createPost);
-        createPost.setOnClickListener(new View.OnClickListener(){
+        ImageView createPost = findViewById(R.id.createPost);
+        createPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(HomePage.this, ShowMapActivity.class);
@@ -59,8 +98,8 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
-        ImageView favorites = (ImageView) findViewById(R.id.favorites);
-        favorites.setOnClickListener(new View.OnClickListener(){
+        ImageView favorites = findViewById(R.id.favorites);
+        favorites.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(HomePage.this, ShowPostOnMapActivity.class);
@@ -68,6 +107,5 @@ public class HomePage extends AppCompatActivity {
             }
         });
     }
-
 
 }
--- app/src/main/java/com/aueb/urbanarts/LogIn.java
+++ app/src/main/java/com/aueb/urbanarts/LogIn.java
@@ -23,23 +23,39 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LogIn extends AppCompatActivity {
-    EditText mEmail,mPassword;
+    EditText mEmail, mPassword;
     Button mLoginBtn;
-    TextView mCreateBtn,forgotTextLink;
+    TextView mCreateBtn, forgotTextLink, tv_guest;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     String userID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        fAuth = FirebaseAuth.getInstance();
+        if (fAuth.getCurrentUser() != null) {
+            Intent myIntent = new Intent(LogIn.this, HomePage.class);
+            LogIn.this.startActivity(myIntent);
+            finish();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
         mEmail = findViewById(R.id.usernameEmail);
         mPassword = findViewById(R.id.password);
-        fAuth = FirebaseAuth.getInstance();
         mLoginBtn = findViewById(R.id.signIn);
         forgotTextLink = findViewById(R.id.forgotPassword);
-        mCreateBtn=findViewById(R.id.signUp);
+        mCreateBtn = findViewById(R.id.signUp);
+        tv_guest = findViewById(R.id.guest);
+
+        tv_guest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LogIn.this, HomePage.class);
+                LogIn.this.startActivity(myIntent);
+            }
+        });
 
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,34 +63,33 @@ public class LogIn extends AppCompatActivity {
                 final String email = mEmail.getText().toString().trim();
                 final String password = mPassword.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required.");
                     return;
                 }
 
-                if(TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
                     mPassword.setError("Password is Required.");
                     return;
                 }
 
-                if(password.length() < 8){
+                if (password.length() < 8) {
                     mPassword.setError("Password Must be >= 8 Characters");
                     return;
                 }
 
 
-
                 // authenticate the user
 
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             userID = fAuth.getCurrentUser().getUid();
-                            fStore.collection("users").document(userID).update("password",password);
+                            fStore.collection("users").document(userID).update("password", password);
                             Toast.makeText(LogIn.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),HomePage.class));
-                        }else {
+                            startActivity(new Intent(getApplicationContext(), HomePage.class));
+                        } else {
                             Toast.makeText(LogIn.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
 
@@ -87,7 +102,7 @@ public class LogIn extends AppCompatActivity {
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),SignUp.class));
+                startActivity(new Intent(getApplicationContext(), SignUp.class));
             }
         });
 
