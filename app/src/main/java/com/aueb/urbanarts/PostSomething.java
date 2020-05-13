e83c90cfb1d4785a2b53924690b89888a44c0d88
geopatrikis <geopatrikis12@gmail.com>
2020-05-14 02:41:39 +0300

    New Activities

--- app/src/main/java/com/aueb/urbanarts/PostSomething.java
+++ app/src/main/java/com/aueb/urbanarts/PostSomething.java
@@ -1,16 +1,56 @@
 package com.aueb.urbanarts;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.WindowManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostSomething extends AppCompatActivity {
-
+String TAG;
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.make_post);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        final List<String> genres =new ArrayList<String>();
+        fStore.collection("genre")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                genres.add(document.getId()+"");
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, genres);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.genresspinner);
+        sItems.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 }
