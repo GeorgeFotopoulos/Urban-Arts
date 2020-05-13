36ac74048bb7f9a4d7405ccc4496e43f2e75b241
GeorgeFotopoulos <giorgos.fotopoulos7@gmail.com>
2020-05-13 15:25:06 +0300

    Register Authentication added

--- app/src/main/java/com/aueb/urbanarts/LogIn.java
+++ app/src/main/java/com/aueb/urbanarts/LogIn.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogIn extends AppCompatActivity {
+    //private FirebaseAuth mAuth;
+
+    //public LogIn(FirebaseAuth mAuth) {
+    //    this.mAuth = mAuth;
+    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //mAuth = FirebaseAuth.getInstance();
     }
+
+    //@Override
+    //public void onStart() {
+    //    super.onStart();
+    //    // Check if user is signed in (non-null) and update UI accordingly.
+    //    FirebaseUser currentUser = mAuth.getCurrentUser();
+    //    //updateUI(currentUser);
+    //}
 }
--- app/src/main/java/com/aueb/urbanarts/SignUp.java
+++ app/src/main/java/com/aueb/urbanarts/SignUp.java
@@ -1,14 +1,130 @@
 package com.aueb.urbanarts;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    TextView tv_username, tv_email, tv_password;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    Button btn_createAccount;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        tv_username = findViewById(R.id.username);
+        tv_email = findViewById(R.id.email);
+        tv_password = findViewById(R.id.password);
+        btn_createAccount = findViewById(R.id.createAccount);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), HomePage.class));
+            finish();
+        }
+
+        btn_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = tv_username.getText().toString().trim();
+                final String email = tv_email.getText().toString().trim();
+                final String password = tv_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username)) {
+                    tv_username.setError("Username is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(email)) {
+                    tv_email.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    tv_password.setError("Password is required.");
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    tv_password.setError("Password must be >= 8 characters.");
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // send verification link
+                            Log.d(TAG, "mphke1");
+                            FirebaseUser fuser = mAuth.getCurrentUser();
+                            Log.d(TAG, fuser.toString());
+                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(SignUp.this, "Verification email has been sent.", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+                                }
+                            });
+                            Toast.makeText(SignUp.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "mphke2");
+                            userID = mAuth.getCurrentUser().getUid();
+                            Log.d(TAG, userID);
+                            //DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("username", username);
+                            user.put("email", email);
+                            user.put("password", password);
+                            //documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            //    @Override
+                            //    public void onSuccess(Void aVoid) {
+                            //        Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                            //    }
+                            //}).addOnFailureListener(new OnFailureListener() {
+                            //    @Override
+                            //    public void onFailure(@NonNull Exception e) {
+                            //        Log.d(TAG, "onFailure: " + e.toString());
+                            //    }
+                            //});
+                            startActivity(new Intent(getApplicationContext(), HomePage.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUp.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+            }
+        });
     }
 }
