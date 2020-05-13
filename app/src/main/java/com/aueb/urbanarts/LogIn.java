cd692a6169291d5e045f90fa2319a983e930958e
geopatrikis <geopatrikis12@gmail.com>
2020-05-13 19:27:54 +0300

    UI filters

--- app/src/main/java/com/aueb/urbanarts/LogIn.java
+++ app/src/main/java/com/aueb/urbanarts/LogIn.java
@@ -20,14 +20,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LogIn extends AppCompatActivity {
     EditText mEmail,mPassword;
     Button mLoginBtn;
     TextView mCreateBtn,forgotTextLink;
     FirebaseAuth fAuth;
-
-
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +70,8 @@ public class LogIn extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            userID = fAuth.getCurrentUser().getUid();
+                            fStore.collection("users").document(userID).update("password",password);
                             Toast.makeText(LogIn.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(),HomePage.class));
                         }else {
